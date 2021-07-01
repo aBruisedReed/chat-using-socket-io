2b1aec2e6cf345a765db409b09dda7c3b28bd6b6
aBruisedReed <snare909@gmail.com>
2021-07-01 22:09:28 +0900

    접속 중인 유저 리스트 구현

--- server.js
+++ server.js
@@ -13,26 +13,38 @@ app.get('/', (req, res) => { // /접속 시 render chat.pug
 });
 
 var count=1; // count users
+var list = [];
 
 io.on('connection', function(socket) { 
   var name = "익명" + count++;
   socket.name = name;
   console.log('user connected: ', socket.id, socket.name);
   var msg = '&lt;Notice&gt; '+socket.name+' is connected.';
+  list = list.concat(socket.name);
   io.emit('receive message', msg);
   io.to(socket.id).emit('create name', name); // 왜 socket.emit안하고?
+
+  io.emit('receive list', list);
 
   socket.on('change name', function(name) {
     console.log('changed name', socket.name, name);
     var msg = '&lt;Notice&gt; '+socket.name+' changed to '+name+'.';
+    list = list.map((item) => {
+      return item === socket.name ? name : item;
+    });
     socket.name = name;
     io.emit('receive message', msg);
+    io.emit('receive list', list);
   })
 
   socket.on('disconnect', function() {
     console.log('user disconnected: ' + socket.id + ' ' + socket.name);
     var msg = '&lt;Notice&gt; '+socket.name+' is disconnected.';
     io.emit('receive message', msg);
+    list = list.filter((item) => {
+      return item !== socket.name;
+    })
+    io.emit('receive list', list);
   });
 
   socket.on('send message', function(name, text) {
